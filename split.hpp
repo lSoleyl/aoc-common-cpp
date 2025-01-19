ce8756a0a80b28f0959c8d505c6209f8b94ddda0
lSoleyl <lSoleyl+github@gmail.com>
2025-01-19 12:00:51 +0100

    Moved string_view::split to common::split and added proper lifetime management in case split() is called with a temporary std::string.

---
+++ split.hpp
@@ -0,0 +1,85 @@
+#include <string_view>
+#include <string>
+
+/** An improved version of the previous string_view::split() function, which didn't work on temporary std::string
+ *  values. If a temporary std::string is passed in this version will move it into the internal range and keep it valid for the
+ *  duration of the iteration.
+ */
+namespace common {
+  namespace impl {
+    template<typename Source, typename SplitByType> requires std::constructible_from<std::string_view, Source>
+    struct StringViewSplit {
+      StringViewSplit(Source&& source, SplitByType splitBy) : source(std::forward<Source>(source)), splitBy(splitBy) {}
+
+      struct iterator {
+        using element_type = std::string_view;
+        using reference_type = const std::string_view&;
+        using pointer_type = const std::string_view*;
+        using iterator_category = std::forward_iterator_tag;
+        using iterator_concept = std::forward_iterator_tag;
+        using difference_type = ptrdiff_t;
+
+        iterator(std::string_view source, SplitByType splitBy) : source(source), splitBy(splitBy) {
+          ++(*this); // position onto first split
+        }
+
+        iterator& operator++() {
+          if (separatorEndPos == std::string_view::npos) {
+            completed = true;
+            return *this;
+          }
+
+          auto pos = source.find(splitBy, separatorEndPos);
+          if (pos == std::string_view::npos) {
+            // No more separators found
+            subMatch = source.substr(separatorEndPos);
+            separatorEndPos = pos;
+          } else {
+            // Found another separator
+            subMatch = source.substr(separatorEndPos, pos - separatorEndPos);
+            separatorEndPos = pos;
+            if constexpr (std::is_same_v<SplitByType, char>) {
+              ++separatorEndPos; // increment past the separator char
+            } else {
+              separatorEndPos += std::string_view(splitBy).size(); // string_view or char* separator
+            }
+          }
+
+          return *this;
+        }
+
+        element_type operator*() const { return subMatch; }
+
+        struct sentinel {};
+
+        bool operator==(sentinel) const { return completed; }
+        bool operator!=(sentinel) const { return !completed; }
+
+        std::string_view source;
+        std::string_view subMatch;
+        SplitByType splitBy;
+        size_t separatorEndPos = 0;
+        bool completed = false;
+      };
+
+      auto begin() const { return iterator(source, splitBy); }
+      auto end() const { return iterator::sentinel(); }
+
+      SplitByType splitBy;
+      Source source; // Usually one of std::string, std::string&, std::string_view, std::string_view&, but also const char* is possible
+    };
+  }
+
+
+
+  template<typename Source> requires std::constructible_from<std::string_view, Source>
+  auto split(Source&& source, char splitChar) {
+    return impl::StringViewSplit<Source, char>(std::forward<Source>(source), splitChar);
+  }
+
+  template<typename Source> requires std::constructible_from<std::string_view, Source>
+  auto split(Source&& source, std::string_view splitString) {
+    return impl::StringViewSplit<Source, std::string_view>(std::forward<Source>(source), splitString);
+  }
+}
+

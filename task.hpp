1e249449f997f7997ff6750a91569c566cd847a5
lSoleyl <lSoleyl+github@gmail.com>
2025-12-10 20:41:10 +0100

    Rewrote task.hpp to not include windows.h anymore and thus not pollute the namespace anymore.

--- task.hpp
+++ task.hpp
@@ -6,16 +6,19 @@
 #include <iostream>
 #include <string_view>
 
-#define NOMINMAX
-#include "windows.h"
+// Manually declare the only used WinAPI function here to avoid including all of windows.h and polluting our namespace just for this.
+extern "C" {
+  __declspec(dllimport) unsigned long __stdcall GetModuleFileNameA(void* hModule, char* path, unsigned long size);
+}
+
 
 namespace task {
-
   /** Retrieves the task id from the name of the executable by stripping the path and the extension
    */
   std::string id() {
+    const unsigned long MAX_PATH = 260;
     char path[MAX_PATH];
-    std::string_view pathView(path, GetModuleFileNameA(NULL, path, MAX_PATH));
+    std::string_view pathView(path, GetModuleFileNameA(nullptr, path, MAX_PATH));
     pathView.remove_prefix(pathView.find_last_of('\\') + 1); // assumption: we always get a path
     pathView.remove_suffix(4); // .exe
     return std::string(pathView);
